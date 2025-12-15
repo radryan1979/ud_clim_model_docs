b512df1a6f3e37b82b8bf1dad4cea5cbca959f6c
Ryan Eagan <ryan.eagan@icloud.com>
2025-12-15 10:42:38 -0500

    Working script, still needs logging and error checking

--- SCRIPTS/era5_data_download.py
+++ SCRIPTS/era5_data_download.py
@@ -34,22 +34,88 @@ logging.basicConfig(
     format="%(asctime)s ]%(levelname)s %(message)s"
     )
 
-def get_pres_data(thedate,hours,download_folder,client):
+def get_pres_data(data,client):
     '''
     Downloads the pressure level data as a grib file by day.
     
     '''
     collection_id = 'reanalysis-era5-pressure-levels'
-        
+    
+    request = {
+            "product_type": ["reanalysis"],
+            "variable": [
+                "geopotential",
+                "relative_humidity",
+                "specific_humidity",
+                "temperature",
+                "u_component_of_wind",
+                "v_component_of_wind"
+            ],
+            "year": data["year"],
+            "month": data["month"],
+            "day": data["day"],
+            "time": data["time"],
+            "data_format": data["data_format"],
+            "download_format": "unarchived",
+            "area": data["area"]
+            }       
+    filename = f"pres_data_{data["year"]}.grib"
+    filename = os.path.join(data["download_folder"],filename)
+    
+    client.retrieve(collection_id, request, target=filename)    
 
-def get_surf_data(thedate,hours,download_folder,client):
+def get_surf_data(data,client):
     '''
     Downloads the surface data as a grib file by day.
     '''
     collection_id = 'reanalysis-era5-single-levels'
+    
+    request = {
+            "product_type": ["reanalysis"],
+            "variable": [
+                "10m_u_component_of_wind",
+                "10m_v_component_of_wind",
+                "2m_dewpoint_temperature",
+                "2m_temperature",
+                "land_sea_mask",
+                "mean_sea_level_pressure",
+                "sea_ice_cover",
+                "sea_surface_temperature",
+                "skin_temperature",
+                "snow_depth",
+                "soil_temperature_level_1",
+                "soil_temperature_level_2",
+                "soil_temperature_level_3",
+                "soil_temperature_level_4",
+                "surface_pressure",
+                "volumetric_soil_water_layer_1",
+                "volumetric_soil_water_layer_2",
+                "volumetric_soil_water_layer_3",
+                "volumetric_soil_water_layer_4",
+                "total_cloud_cover",
+                "total_precipitation",
+                "surface_latent_heat_flux",
+                "top_net_solar_radiation_clear_sky",
+                "temperature_of_snow_layer",
+                "soil_type",
+                "leaf_area_index_high_vegetation"
+            ],
+            "year": data["year"],
+            "month": data["month"],
+            "day": data["day"],
+            "time": data["time"],
+            "data_format": data["data_format"],
+            "download_format": "unarchived",
+            "area": data["area"]
+            }       
+    
+    filename = f"surf_data_{data["year"]}.grib"
+    filename = os.path.join(data["download_folder"],filename)
+        
+    client.retrieve(collection_id, request, target=filename)
 
 def main():
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 2:
         print("Usage: python script.py <JSON_file>")
         sys.exit(1)
 
@@ -68,9 +134,9 @@ def main():
     
 
     # Normalize folder path
-    folder = os.path.abspath(data.download_folder)
+    folder = os.path.abspath(data["download_folder"])
     folder = Path(folder)
-    data.download_folder = folder
+    data["download_folder"] = folder
 
     logging.info(f"Folder entered: {folder}")
     
